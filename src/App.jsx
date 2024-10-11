224ab7402632649ae1a10fe686b3e872daf8ec71
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-10-12 00:07:14 +0330

    add Screen

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ const TodoList = () => {
   return (
     <div>
       <div className="flex items-center justify-center ">
-        <div className="w-full px-8 py-8 mx-auto shadow lg:w-1/3  bg-white">
+        <div className="w-full mt-10 px-8 py-8 mx-auto shadow lg:w-1/3  bg-white">
           <div className="flex items-center mb-6">
             <h1 className="mr-6 text-4xl font-bold text-purple-600">
               Todo List
